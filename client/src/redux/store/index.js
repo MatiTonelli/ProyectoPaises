e223484300f571baa7d13cad12329e8a1e3e17ee
MatiTonelli <matias.tonelli@outlook.com>
2022-10-04 21:17:49 -0300

    redux update

--- client/src/redux/store/index.js
+++ client/src/redux/store/index.js
@@ -1,10 +1,9 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import rootReducer from "../reducers/index";
-import thunk from "redux-thunk";
+import { legacy_createStore as createStore, applyMiddleware } from 'redux';
+import {composeWithDevTools} from "redux-devtools-extension"
+import rootReducer from '../reducers/index.js';
+import thunk from 'redux-thunk';
 
-const store = createStore(rootReducer, 
-    compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-    );
 
+const store = createStore(rootReducer,composeWithDevTools(applyMiddleware(thunk)),);
 
 export default store;
